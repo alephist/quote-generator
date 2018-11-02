9810b92176e858e5e7015c7ad72aa3f1d3299dc4
Justin Necesito <justin.necesito@gmail.com>
2018-11-03 01:38:32 +0800

    Added quote card component

--- quote.js
+++ quote.js
@@ -1,30 +1,57 @@
+// Card Component
+Vue.component('quote-card', {
+  props: {
+    list: Array
+  },
+  template: `
+    <div class="card" v-if="this.list.length">
+      <div class="card-header">
+        <p class="card-header-title is-centered">Category: {{ quoteCategory }}</p>
+      </div>
+      <div class="card-content">
+        <p class="title is-size-4">"{{ quoteTitle }}"</p>
+        <p class="subtitle has-text-right">- {{ quoteAuthor }}</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="card-footer-item" @click="changeQuote">Get Quote</a>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      quoteIndex: 0
+    };
+  },
+  methods: {
+    changeQuote() {
+      this.quoteIndex = Math.floor(Math.random() * this.list.length);
+    }
+  },
+  computed: {
+    quoteTitle() {
+      return this.list[this.quoteIndex].quote;
+    },
+    quoteAuthor() {
+      return this.list[this.quoteIndex].author;
+    },
+    quoteCategory() {
+      let { cat } = this.list[this.quoteIndex];
+      return cat.charAt(0).toUpperCase() + cat.slice(1);
+    }
+  }
+});
+
 const app = new Vue({
   el: '#app',
   data: {
-    quoteList: [],
-    quoteIndex: 0
+    quoteList: []
   },
   methods: {
     getQuotes() {
       axios.get('https://talaikis.com/api/quotes/').then((response) => (this.quoteList = response.data));
-    },
-    changeQuote() {
-      this.quoteIndex = Math.floor(Math.random() * this.quoteList.length);
     }
   },
   created() {
     this.getQuotes();
-  },
-  computed: {
-    quoteTitle() {
-      return this.quoteList[this.quoteIndex].quote;
-    },
-    quoteAuthor() {
-      return this.quoteList[this.quoteIndex].author;
-    },
-    quoteCategory() {
-      let { cat } = this.quoteList[this.quoteIndex];
-      return cat.charAt(0).toUpperCase() + cat.slice(1);
-    }
   }
 });
